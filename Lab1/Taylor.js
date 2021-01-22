1a56eed6b6416ff93bd4ffdb2a2cd17cecbb7a81
taytho1 <thompson.taylor97@knights.ucf.edu>
2021-01-21 23:35:02 -0500

    Lab1

---
+++ Lab1/Taylor.js
@@ -0,0 +1,8 @@
+const Person = require("./Person.js");
+
+class Taylor extends Person {
+    constructor(name,color){
+        super(name,color);
+    }
+}
+module.exports = Taylor;
---
+++ Lab1/index.js
@@ -0,0 +1,5 @@
+const Taylor = require("./Taylor.js");
+
+let p = new Taylor ("Taylor","#51BEC8");
+
+p.speak();
