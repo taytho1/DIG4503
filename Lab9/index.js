2406d120f1b977a249d296306d532629105a0ab2
taytho1 <thompson.taylor97@knights.ucf.edu>
2021-03-26 12:41:11 -0400

    Lab9

--- Lab9/index.js
+++ Lab9/index.js
@@ -12,8 +12,10 @@ let fileContents = fs.readFileSync("database.json")
 //converting the file to json
 let database = JSON.parse(fileContents);
 
+App.use("/", Express.static("public"));
+
 //get the names of each employee
-App.get('/employees/:name', (req, res) => {
+App.get('/employees/name/:name', (req, res) => {
     //error message
     let result = {"error": "Not found"};
 
@@ -29,13 +31,13 @@ App.get('/employees/:name', (req, res) => {
 })
 
 //get the ages of the employees
-App.get('/ages/:number', (req, res) => {
+App.get('/employees/age/:age', (req, res) => {
     //error message
     let result = {"error": "Not found"};
   
     //if the age entered matches to an employees age
     database.forEach((value) => {
-        if(req.params.age == value.age){
+        if (req.params.age == value.age){
             result = value;
         }
     });
@@ -44,6 +46,19 @@ App.get('/ages/:number', (req, res) => {
     res.json(result);
     
 })
+//posting the new names and ages to the database
+App.post("/employees/:name/:age", (req, res) => {
+    let result = {
+        "name": req.params.name,
+        "age": parseInt(req.params.age)
+    };
+
+    database.push(result);
+
+    fs.writeFileSync("database.json", JSON.stringify(database, null, '\t'));
+
+    res.json(result);
+});
 
 //listening on the port previously defined 
 App.listen(port, () => {
