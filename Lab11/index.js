5ed38e78de33b6eb079df6cee277b876368d8f51
taytho1 <thompson.taylor97@knights.ucf.edu>
2021-04-09 18:38:47 -0400

    Lab11

---
+++ Lab11/index.js
@@ -0,0 +1,26 @@
+//importing mongodb
+import MongoClient from "mongodb";
+
+//my url login
+const URL = "mongodb+srv://tthompson:4ve9uR6qGd9EGnkj@cluster0.oewwc.mongodb.net/";
+
+//making all the connections and defining the search parameters
+MongoClient.connect(URL, {useUnifiedTopology: true})
+.then(connection =>{
+    let database = connection.db("sample_airbnb");
+
+    let collection = database.collection("listingsAndReviews");
+
+    let cursor = collection.find({beds: {$gte: 5}}, {price: {$lte: 200}}, {"review_scores.review_scores_rating": {$gte: 99 }});
+
+//closing the search
+    cursor.forEach(document => {
+        console.log(document.name);
+    }, () =>{
+        connection.close();
+    })
+})
+//catching errors
+.catch(error =>{
+    console.log("Error: " + error);
+});
